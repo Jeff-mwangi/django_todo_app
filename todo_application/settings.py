994247b284a57a6e5cb41b467fa296f27e808222
Jeff-mwangi <jeffmwangi442@gmail.com>
2022-09-24 00:45:03 +0300

    minor changes

--- todo_application/settings.py
+++ todo_application/settings.py
@@ -12,10 +12,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
-import environ
-env = environ.Env()
-# reading .env file
-environ.Env.read_env()
+from dotenv import load_dotenv   #for python-dotenv method
+load_dotenv() 
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -153,11 +152,11 @@ WSGI_APPLICATION = 'todo_application.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env("DATABASE_NAME"),
-        'USER': env("DATABASE_USER"),
-        'PASSWORD': env("DATABASE_PASSWORD"),
-        'HOST': env("DATABASE_HOST"),
-        'PORT': env("DATABASE_PORT"),
+        'NAME':  os.environ.get("DATABASE_NAME"),
+        'USER':  os.environ.get("DATABASE_USER"),
+        'PASSWORD':  os.environ.get("DATABASE_PASSWORD"),
+        'HOST':  os.environ.get("DATABASE_HOST"),
+        'PORT':  os.environ.get("DATABASE_PORT"),
     }
 }
 
@@ -213,8 +212,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER =  os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD =  os.environ.get('EMAIL_HOST_PASSWORD')
 
 LOGIN_URL = 'login'
 
--- users/urls.py
+++ users/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('password_reset/',auth_views.PasswordResetView.as_view(template_name="users/password_reset.html"), name= 'password_reset'),
     path('password_reset_confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name="users/password_reset_confirm.html"), name= 'password_reset_confirm'),
     path('password_reset/done/',auth_views.PasswordResetDoneView.as_view(template_name="users/password_reset_done.html"), name= 'password_reset_done'),
+    path('password_reset/complete/',auth_views.PasswordResetCompleteView.as_view(template_name="users/password_reset_complete.html"), name= 'password_reset_complete'),
     path('update_profile/',views.update_profile, name= 'update-profile'),
 ]
